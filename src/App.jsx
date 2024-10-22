09dce81e5e6dcf81205faa543564bfba386f2a5d
marina-nh <herediamarinarocio@gmail.com>
2024-10-21 23:26:10 -0300

    Added config

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3x1 font-bold'>
-        Hello word!
+      <h1 className='text-3x1 font-bold text-neutralDGrey'>
+
       </h1>
     </>
-  );
+  );  
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'neutralSilver': '#F5F7FA',
         'neutralDGrey': '#4D4D4D',
         'brandPrimary': '#4CAF4F',
-        'neutralSilver': '#F5F7FA ',
+        'neutralGrey': '#717171',
       }
     },
   },
